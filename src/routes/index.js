c9d32f47ebbb62e494156af703f58fcf1d3c95e7
spring8904 <lamthang890123music@gmail.com>
2024-10-29 10:25:57 +0700

    feat: ping route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
+import { setupSwaggerDocs } from '../swagger'
 import authRouter from './auth.routes'
+import cartRouter from './cart.routes'
 import cloudinaryRouter from './cloudinary.routes'
 import productRouter from './product.routes'
-import cartRouter from './cart.routes'
 
 const router = express.Router()
 
@@ -10,5 +11,11 @@ router.use('/auth', authRouter)
 router.use('/products', productRouter)
 router.use('/cart', cartRouter)
 router.use('/cloudinary', cloudinaryRouter)
+
+setupSwaggerDocs(router)
+
+router.use('/ping', (req, res) => {
+  res.send('pong')
+})
 
 export default router
