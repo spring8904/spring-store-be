0443a5c76087868c6cf49d712454fc2feea72251
spring8904 <lamthang890123music@gmail.com>
2024-10-29 10:28:06 +0700

    docs: Add swagger documentation for API endpoints

---
+++ src/swagger.js
@@ -0,0 +1,25 @@
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import { version } from '../package.json'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version,
+    },
+    servers: [
+      {
+        url: import.meta.env.VITE_SERVER_URL,
+      },
+    ],
+  },
+  apis: ['./src/docs/*.yaml'],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+export const setupSwaggerDocs = (app) => {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+}
