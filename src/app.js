ed1febb8ebcfdbf450ee47d423a7f89c7d1e39b4
spring8904 <lamthang890123music@gmail.com>
2024-11-04 19:29:31 +0700

    refactor: improve response status handling in app.js and index.js

--- src/app.js
+++ src/app.js
@@ -13,9 +13,13 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.get('/', (req, res) => res.json('Welcome to Spring Store API'))
+app.get('/', (req, res) => {
+  res.status(StatusCodes.OK).send('Welcome to Spring Store API')
+})
 app.use('/api', router)
-app.use((req, res) => res.status(StatusCodes.NOT_FOUND).json('Route not found'))
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).send('Route not found')
+})
 
 app.listen(PORT)
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { StatusCodes } from 'http-status-codes'
 import { setupSwaggerDocs } from '../swagger'
 import authRouter from './auth.routes'
 import cartRouter from './cart.routes'
@@ -15,7 +16,7 @@ router.use('/cloudinary', cloudinaryRouter)
 setupSwaggerDocs(router)
 
 router.use('/ping', (req, res) => {
-  res.send('pong')
+  res.status(StatusCodes.OK).send('pong')
 })
 
 export default router
